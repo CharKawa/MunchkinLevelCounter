6a76508f961164c0b380e254d3517dbb9ff9f875
Artem Gulia <artem.gulia@gmail.com>
2014-11-07 17:09:13 +0200

    Update PurchaseListener.java

--- app/src/main/java/com/g_art/munchkinlevelcounter/billing/PurchaseListener.java
+++ app/src/main/java/com/g_art/munchkinlevelcounter/billing/PurchaseListener.java
@@ -10,11 +10,22 @@ import org.solovyev.android.checkout.RequestListener;
 public class PurchaseListener implements RequestListener<Purchase> {
     @Override
     public void onSuccess(Purchase purchase) {
-
+        onPurchased();
+    }
+    
+    
+    public void onPurchased(){
+        //TODO: create R.string.msg_thank_you_for_purchase
+        Toast.makeText(getActivity(), R.string.msg_thank_you_for_purchase, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onError(int i, Exception e) {
-
+        // it is possible that our data is not synchronized with data on Google Play => need to handle some errors
+			if (i == ResponseCodes.ITEM_ALREADY_OWNED) {
+				onPurchased();
+			} else {
+				super.onError(i, e);
+			}
     }
 }
