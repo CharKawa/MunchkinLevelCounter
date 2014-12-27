d79d374e4cffa413cf93f549464a2ad2be90ee81
artem.gulia <artem.gulia@gmail.com>
2014-12-27 11:56:29 +0200

    fix rate button for amazon store

--- app/src/main/java/com/g_art/munchkinlevelcounter/activity/About.java
+++ app/src/main/java/com/g_art/munchkinlevelcounter/activity/About.java
@@ -1,12 +1,10 @@
 package com.g_art.munchkinlevelcounter.activity;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -37,12 +35,13 @@ public class About extends Activity implements View.OnClickListener {
         switch (v.getId()) {
             case R.id.btn_Rate:
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                //Try Google Play
-                intent.setData(Uri.parse("market://details?id=com.g_art.munchkinlevelcounter"));
+                //Try Amazon market
+                intent.setData(Uri.parse("amzn://apps/android?p=com.g_art.munchkinlevelcounter"));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 
                 if (!MyStartActivity(intent)) {
-                    //Market (Google play) app seems not installed, let's try to open a webbrowser
-                    intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.g_art.munchkinlevelcounter"));
+                    //Market (Amazon market) app seems not installed, let's try to open a webbrowser
+                    intent.setData(Uri.parse("http://www.amazon.com/gp/mas/dl/android?p=com.g_art.munchkinlevelcounter"));
                     if (!MyStartActivity(intent)) {
                         //Well if this also fails, we have run out of options, inform the user.
                         Toast.makeText(this, "Could not open Android market, please install the market app.", Toast.LENGTH_SHORT).show();
