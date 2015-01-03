84464687bfa84ec16a1c22c65d17da909040ce70
artem.gulia <artem.gulia@gmail.com>
2015-01-03 16:20:07 +0200

    Handle max level edit text

--- app/src/main/java/com/g_art/munchkinlevelcounter/activity/Settings.java
+++ app/src/main/java/com/g_art/munchkinlevelcounter/activity/Settings.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.g_art.munchkinlevelcounter.R;
 import com.g_art.munchkinlevelcounter.settings.SettingsHandler;
@@ -13,9 +17,10 @@ import com.g_art.munchkinlevelcounter.settings.SettingsHandler;
 /**
  * Created by G_Art on 1/8/2014.
  */
-public class Settings extends Activity implements CompoundButton.OnCheckedChangeListener {
+public class Settings extends Activity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
 
     private Switch statsSwitch;
+    private EditText edTextMaxLvl;
     private SharedPreferences mPrefs;
     private SettingsHandler setHandler;
 
@@ -29,18 +34,70 @@ public class Settings extends Activity implements CompoundButton.OnCheckedChange
         setHandler = SettingsHandler.getInstance(mPrefs);
 
         statsSwitch = (Switch) findViewById(R.id.switchStats);
-        statsSwitch.setChecked(setHandler.loadSettings());
+        edTextMaxLvl = (EditText) findViewById(R.id.edTextMaxLvl);
+        if (setHandler.loadSettings()) {
+            statsSwitch.setChecked(setHandler.isStats());
+            edTextMaxLvl.setText(Integer.toString(setHandler.getMaxLvl()), TextView.BufferType.EDITABLE);
+        }
 
         statsSwitch.setOnCheckedChangeListener(this);
+        edTextMaxLvl.setOnClickListener(this);
     }
 
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if (isChecked) {
-            setHandler.saveSettings(true);
-        } else {
-            setHandler.saveSettings(false);
+        try {
+            saveSettings(isChecked, parseLevel());
+            Toast.makeText(this,
+                    getString(R.string.settings_saved),
+                    Toast.LENGTH_SHORT
+            ).show();
+        } catch (NumberFormatException ex) {
+            Toast.makeText(this,
+                    getString(R.string.error_max_lvl_settings),
+                    Toast.LENGTH_SHORT
+            ).show();
+            edTextMaxLvl.setText(Integer.toString(setHandler.getMaxLvl()), TextView.BufferType.EDITABLE);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.edTextMaxLvl:
+                try {
+                    saveSettings(statsSwitch.isChecked(), parseLevel());
+                    Toast.makeText(this,
+                            getString(R.string.settings_saved),
+                            Toast.LENGTH_SHORT
+                    ).show();
+                    break;
+                } catch (NumberFormatException ex) {
+                    Toast.makeText(this,
+                            getString(R.string.error_max_lvl_settings),
+                            Toast.LENGTH_SHORT
+                    ).show();
+                    edTextMaxLvl.setText(Integer.toString(setHandler.getMaxLvl()), TextView.BufferType.EDITABLE);
+                }
+
+        }
+    }
+
+    private int parseLevel() throws NumberFormatException {
+        int mLvl = Integer.parseInt(edTextMaxLvl.getText().toString());
+        if (mLvl < SettingsHandler.MIN_LVL) {
+            Toast.makeText(this,
+                    getString(R.string.error_max_level_one),
+                    Toast.LENGTH_SHORT
+            ).show();
+            mLvl = setHandler.getMaxLvl();
+            edTextMaxLvl.setText(mLvl, TextView.BufferType.EDITABLE);
+        }
+        return mLvl;
+    }
+
+    private void saveSettings(boolean isChecked, int maxLvl) {
+        setHandler.saveSettings(isChecked, maxLvl);
     }
 }
