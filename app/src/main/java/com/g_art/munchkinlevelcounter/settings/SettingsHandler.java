8835a7be6e698c4cca38d6aedc68cef349eefa88
artem.gulia <artem.gulia@gmail.com>
2015-01-03 16:21:10 +0200

    New data to storing in shared prefs

--- app/src/main/java/com/g_art/munchkinlevelcounter/settings/SettingsHandler.java
+++ app/src/main/java/com/g_art/munchkinlevelcounter/settings/SettingsHandler.java
@@ -7,9 +7,17 @@ import android.content.SharedPreferences;
  */
 public class SettingsHandler {
     private static SettingsHandler settingsInstance;
+
     public static final String STATS_SETTINGS = "stats_settings";
+    public static final String MAX_LVL_SETTINGS = "max_lvl_settings";
+    public static final int DEFAULT_MAX_LVL = 10;
+    public static final int MIN_LVL = 1;
+
     private SharedPreferences mPrefs;
     private SharedPreferences.Editor prefsEditor;
+
+    private boolean isStats = false;
+    private int maxLvl = 10;
 
     public static SettingsHandler getInstance(SharedPreferences mPrefs) {
         if (settingsInstance == null) {
@@ -24,16 +32,20 @@ public class SettingsHandler {
 
 
     public boolean loadSettings() {
-        if (!mPrefs.contains(STATS_SETTINGS)) {
+        if (!mPrefs.contains(STATS_SETTINGS) || !mPrefs.contains(MAX_LVL_SETTINGS)) {
             saveDefaultSettings();
         }
-        boolean result = mPrefs.getBoolean(STATS_SETTINGS, true);
+
+        isStats = mPrefs.getBoolean(STATS_SETTINGS, true);
+        maxLvl = mPrefs.getInt(MAX_LVL_SETTINGS, DEFAULT_MAX_LVL);
+        boolean result = true;
         return result;
     }
 
-    public boolean saveSettings(boolean checked) {
+    public boolean saveSettings(boolean isChecked, int maxLvl) {
         prefsEditor = mPrefs.edit();
-        prefsEditor.putBoolean(STATS_SETTINGS, checked);
+        prefsEditor.putBoolean(STATS_SETTINGS, isChecked);
+        prefsEditor.putInt(MAX_LVL_SETTINGS, maxLvl);
         boolean result;
         try {
             prefsEditor.apply();
@@ -44,7 +56,27 @@ public class SettingsHandler {
         return result;
     }
 
+    public boolean saveSettings() {
+        return saveSettings(isStats(), getMaxLvl());
+    }
+
     public boolean saveDefaultSettings() {
-        return saveSettings(true);
+        return saveSettings(true, DEFAULT_MAX_LVL);
+    }
+
+    public boolean isStats() {
+        return isStats;
+    }
+
+    public void setStats(boolean isStats) {
+        this.isStats = isStats;
+    }
+
+    public int getMaxLvl() {
+        return maxLvl;
+    }
+
+    public void setMaxLvl(int maxLvl) {
+        this.maxLvl = maxLvl;
     }
 }
