5f9cc4f4b470b72a1ee2ce387fadbaa0641d3d4e
artem.gulia <artem.gulia@gmail.com>
2015-01-05 17:35:35 +0200

    Created DiceDialog.java for Dice btn

--- app/src/main/java/com/g_art/munchkinlevelcounter/fragments/dialog/DiceDialog.java
+++ app/src/main/java/com/g_art/munchkinlevelcounter/fragments/dialog/DiceDialog.java
@@ -3,12 +3,14 @@ package com.g_art.munchkinlevelcounter.fragments.dialog;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.g_art.munchkinlevelcounter.R;
+import com.g_art.munchkinlevelcounter.fragments.game.FragmentPlayer;
 
 /**
  * MunchinLevelCounter
@@ -28,7 +30,16 @@ public class DiceDialog extends DialogFragment {
 
         imageView = (ImageView) v.findViewById(R.id.imgDice);
 
-
+        Bundle bundle = getArguments();
+        int resId = bundle.getInt(FragmentPlayer.DICE_KEY);
+        imageView.setImageResource(resId);
+        builder.setTitle(R.string.dice);
+        builder.setPositiveButton(R.string.dialog_ok_btn, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Do nothing
+            }
+        });
         return builder.create();
     }
 }
