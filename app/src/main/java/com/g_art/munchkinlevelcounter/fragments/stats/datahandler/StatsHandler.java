4ab0c90f2d8f8ddf806a9995fd4738ef87550125
artem.gulia <artem.gulia@gmail.com>
2015-02-03 19:45:30 +0200

    Fix indexOutOfBound exception

--- app/src/main/java/com/g_art/munchkinlevelcounter/fragments/stats/datahandler/StatsHandler.java
+++ app/src/main/java/com/g_art/munchkinlevelcounter/fragments/stats/datahandler/StatsHandler.java
@@ -2,8 +2,10 @@ package com.g_art.munchkinlevelcounter.fragments.stats.datahandler;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.SparseArray;
 
+import com.db.chart.Tools;
 import com.db.chart.model.ChartSet;
 import com.db.chart.model.LineSet;
 import com.db.chart.model.Point;
@@ -18,16 +20,20 @@ import java.util.Random;
  * Created by G_Art on 3/9/2014.
  */
 public class StatsHandler {
-    final String TAG = "GameActivity_Munchkin_Test";
     private final static int CLEAR_STRING_BULDER = 0;
     private ArrayList<Player> playersList;
+
     private LineChartView chartView;
+
     private static final Random rnd = new Random();
     private SparseArray<String> playersColorSparse;
     private StringBuilder s;
     public final static int LVL_STATS = 0;
     public final static int GEAR_STATS = 1;
     public final static int POWER_STATS = 2;
+    private int MIN_VALUE = 0;
+    private int MAX_VALUE;
+    private final int STEP = 1;
 
 
     public StatsHandler(ArrayList<Player> playersList, LineChartView chartView) {
@@ -58,29 +64,46 @@ public class StatsHandler {
 
                 switch (stats) {
                     case LVL_STATS:
+                        MIN_VALUE = 1;
                         for (int i = 0; i < player.getLvlStats().size(); i++) {
-                            lineSet.addPoint(new Point(String.valueOf(" "), Integer.parseInt(player.getLvlStats().get(i))));
+
+                            int lvlValue = Integer.parseInt(player.getLvlStats().get(i));
+                            lineSet.addPoint(new Point(String.valueOf(" "), lvlValue));
+
+                            if (MAX_VALUE < lvlValue) {
+                                MAX_VALUE = lvlValue;
+                            }
                         }
                         s.append(" ").append(lvl).append(": ").append(player.getLevel());
                         break;
                     case GEAR_STATS:
                         boolean gearChanged = false;
+                        MIN_VALUE = 0;
                         for (int i = 0; i < player.getGearStats().size(); i++) {
-                            int gearStat = Integer.parseInt(player.getGearStats().get(i));
-                            lineSet.addPoint(new Point(String.valueOf(" "), gearStat));
+                            int gearValue = Integer.parseInt(player.getGearStats().get(i));
+                            lineSet.addPoint(new Point(String.valueOf(" "), gearValue));
 
-                            if (gearStat != 0) {
+                            if (MAX_VALUE < gearValue) {
+                                MAX_VALUE = gearValue;
+                            }
+                            if (gearValue != 0) {
                                 gearChanged = true;
                             }
                         }
                         if (!gearChanged) {
-                            lineSet.addPoint(new Point(String.valueOf(" "), GEAR_STATS));
+                            MAX_VALUE = GEAR_STATS;
                         }
                         s.append(" ").append(gear).append(": ").append(player.getGear());
                         break;
                     case POWER_STATS:
+                        MIN_VALUE = 1;
                         for (int i = 0; i < player.getPowerStats().size(); i++) {
-                            lineSet.addPoint(new Point(String.valueOf(" "), Integer.parseInt(player.getPowerStats().get(i))));
+                            int powerValue = Integer.parseInt(player.getPowerStats().get(i));
+                            lineSet.addPoint(new Point(String.valueOf(" "), powerValue));
+
+                            if (MAX_VALUE < powerValue) {
+                                MAX_VALUE = powerValue;
+                            }
                         }
                         s.append(" ").append(power).append(": ").append(player.getLevel() + player.getGear());
                         break;
@@ -98,16 +121,23 @@ public class StatsHandler {
 
                 // Style background fill
                 lineSet.setFill(false);
+                lineSet.setSmooth(true);
 
-                lineSet.setLineSmooth(true);
                 statLines.add(lineSet);
             }
 
-            chartView.setLabels(false);
-            chartView.setGrid(true);
+            Paint mLineGridPaint = new Paint();
+            mLineGridPaint.setColor(context.getResources().getColor(R.color.text_color));
+            mLineGridPaint.setAntiAlias(true);
+            mLineGridPaint.setStrokeWidth(Tools.fromDpToPx(.10f));
+
+            chartView.setXAxis(true).setYAxis(true).setGrid(LineChartView.GridType.FULL, mLineGridPaint);
+            chartView.setAxisBorderValues(MIN_VALUE, MAX_VALUE, STEP);
+
+            chartView.setFontSize(10);
+
             chartView.addData(statLines);
 
-//            chartView.setAnimation(new Animation(1000));
             return chartView;
         }
 
