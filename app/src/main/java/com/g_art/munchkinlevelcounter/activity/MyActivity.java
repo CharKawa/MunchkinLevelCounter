244211145dc40c05efeea37ad4de260fff55cb2b
artem.gulia <artem.gulia@gmail.com>
2015-12-04 13:24:24 +0200

    Some updates in popup

--- app/src/main/java/com/g_art/munchkinlevelcounter/activity/MyActivity.java
+++ app/src/main/java/com/g_art/munchkinlevelcounter/activity/MyActivity.java
@@ -2,6 +2,8 @@ package com.g_art.munchkinlevelcounter.activity;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -15,6 +17,7 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.g_art.munchkinlevelcounter.R;
+import com.g_art.munchkinlevelcounter.fragments.info.ThanksPopup;
 import com.g_art.munchkinlevelcounter.util.StoredPlayers;
 
 
@@ -32,6 +35,22 @@ public class MyActivity extends Activity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        showDialog();
+    }
+
+    public void showDialog() {
+        FragmentManager fragmentManager = getFragmentManager();
+        ThanksPopup newFragment = new ThanksPopup();
+
+        // The device is smaller, so show the fragment fullscreen
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        // For a little polish, specify a transition animation
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        // To make it fullscreen, use the 'content' root view as the container
+        // for the fragment, which is always the root view for the activity
+        transaction.add(android.R.id.content, newFragment)
+                .addToBackStack(null).commit();
+
     }
 
     @Override
--- app/src/main/java/com/g_art/munchkinlevelcounter/fragments/info/ThanksPopup.java
+++ app/src/main/java/com/g_art/munchkinlevelcounter/fragments/info/ThanksPopup.java
@@ -1,11 +1,12 @@
 package com.g_art.munchkinlevelcounter.fragments.info;
 
-import android.app.Fragment;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 import com.g_art.munchkinlevelcounter.R;
 
@@ -13,15 +14,31 @@ import com.g_art.munchkinlevelcounter.R;
  * LevelCounter
  * Created by G_Art on 1/12/2015.
  */
-public class ThanksPopup extends Fragment implements View.OnClickListener {
-    private View view;
+public class ThanksPopup extends DialogFragment implements View.OnClickListener {
 
-    @Nullable
+    /**
+     * The system calls this to get the DialogFragment's layout, regardless
+     * of whether it's being displayed as a dialog or an embedded fragment.
+     */
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_tanks_popup, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout to use as dialog or embedded fragment
+        return inflater.inflate(R.layout.fragment_tanks_popup, container, false);
+    }
 
-        return view;
+    /**
+     * The system calls this only when creating the layout in a dialog.
+     */
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // The only reason you might override this method when using onCreateView() is
+        // to modify any dialog characteristics. For example, the dialog includes a
+        // title by default, but your custom layout might not need it. So here you can
+        // remove the dialog title, but you must call the superclass to get the Dialog.
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
     }
 
     @Override
