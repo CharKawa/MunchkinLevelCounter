3f544bafdad71a7b90ff4e29e95fb659e481a067
Artem Gulia <agulia@xy-dev-agulia.local>
2017-02-10 14:44:36 +0200

    Fix amazon start crash

--- app/src/amazon/java/com.g_art.munchkinlevelcounter/application/MyApplication.java
+++ app/src/amazon/java/com.g_art.munchkinlevelcounter/application/MyApplication.java
@@ -2,11 +2,10 @@ package com.g_art.munchkinlevelcounter.application;
 
 import android.app.Application;
 
-import com.crashlytics.android.Crashlytics;
 import com.g_art.munchkinlevelcounter.R;
+import com.g_art.munchkinlevelcounter.analytics.Analytics;
 import com.g_art.munchkinlevelcounter.util.SettingsMigration;
 
-import io.fabric.sdk.android.Fabric;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
@@ -23,12 +22,7 @@ public class MyApplication extends Application {
 				.setFontAttrId(R.attr.fontPath)
 				.build()
 		);
-
-		final Fabric fabric = new Fabric.Builder(this)
-				.kits(new Crashlytics())
-				.debuggable(true)
-				.build();
-		Fabric.with(fabric);
+		Analytics.init(this);
 
 		SettingsMigration.startMigration(getBaseContext());
 	}
