90667c946468040a0f10af26c5536fd706d830e4
Artem Gulia <agulia@xy-dev-agulia.local>
2017-02-18 14:02:16 +0200

    fixed migration version

--- app/src/main/java/com/g_art/munchkinlevelcounter/util/SettingsMigration.java
+++ app/src/main/java/com/g_art/munchkinlevelcounter/util/SettingsMigration.java
@@ -34,11 +34,13 @@ public class SettingsMigration {
 			mPrefs.edit().remove(PreferenceScreen.KEY_PREF_MAX_LEVEL).apply();
 
 			mPrefs.edit().putString(PreferenceScreen.KEY_PREF_MAX_LEVEL, Integer.toString(maxLevel)).apply();
+			mPrefs.edit().putInt(SETTINGS_VERSION_KEY, CURRENT_SETTINGS_VERSION).apply();
 		}
 		if (PREV_SETTINGS_VERSION == settingsVersion) {
 			final int maxLevel = mPrefs.getInt(PreferenceScreen.KEY_PREF_MAX_LEVEL, PreferenceScreen.DEFAULT_MAX_LVL);
 			mPrefs.edit().remove(PreferenceScreen.KEY_PREF_MAX_LEVEL).apply();
 			mPrefs.edit().putString(PreferenceScreen.KEY_PREF_MAX_LEVEL, Integer.toString(maxLevel)).apply();
+			mPrefs.edit().putInt(SETTINGS_VERSION_KEY, CURRENT_SETTINGS_VERSION).apply();
 		}
 
 	}
